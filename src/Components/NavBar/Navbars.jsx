bf6922a1a53b90d2f4cb969e6450bd3cdee52d64
vikneshwaranvikey <vikeyviknesh@gmail.com>
2023-12-08 16:24:11 +0530

    changes

--- src/Components/NavBar/Navbars.jsx
+++ src/Components/NavBar/Navbars.jsx
@@ -38,7 +38,7 @@ function Navbars() {
                         <ul className='md:flex md:space-x-8 space-y-4 md:space-y-0 font-semibold'>
                             <li className='animate-pulse bg-red-500 p-1 rounded-lg'><Link to='/'>Home</Link></li>
                             <li>Account</li>
-                            <li>List</li>
+                            <li>WishList</li>
                             <li >Basket</li>
                             <li ><Link to="/AddCart"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
